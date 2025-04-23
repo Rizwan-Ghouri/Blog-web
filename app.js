a23d1dbea50834e61f74a24e8656f515c5f1054e
Rizwan-Ghouri <rizwanidrees138@gmail.com>
2025-04-23 22:50:38 +0500

    create blog web

---
+++ app.js
@@ -0,0 +1,20 @@
+let login_btn = document.getElementById("Login_btn");
+let Signup_btn = document.getElementById("Signup_btn");
+let np_btn = document.getElementById("np_btn");
+let blog_btn = document.getElementById("blog_btn");
+
+login_btn.addEventListener("click" ,()=>{
+    location.href = "page/login.html"
+})
+
+Signup_btn.addEventListener("click" ,()=>{
+    location.href = "page/Signup.html"
+})
+
+np_btn.addEventListener("click" ,()=>{
+    location.href = "page/blog.html"
+})
+
+blog_btn.addEventListener("click" ,()=>{
+    location.href = "page/blog.html"
+})
