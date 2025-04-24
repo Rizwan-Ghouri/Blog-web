87ab797bf2fd8d5e4119161d83df2b494997a3fb
Rizwan-Ghouri <rizwanidrees138@gmail.com>
2025-04-24 22:49:36 +0500

    create firebase & marquee tag

--- app.js
+++ app.js
@@ -18,3 +18,31 @@ np_btn.addEventListener("click" ,()=>{
 blog_btn.addEventListener("click" ,()=>{
     location.href = "page/blog.html"
 })
+
+// Todo : connect firebase
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+import { getFirestore, collection , addDoc  } from "https://www.gstatic.com/firebasejs/11.6.0/firebase/firestore.js";
+// Todo: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDBfllD0_3erohnqzff6scfMENJ_t3tReI",
+  authDomain: "task-project-c94df.firebaseapp.com",
+  projectId: "task-project-c94df",
+  storageBucket: "task-project-c94df.firebasestorage.app",
+  messagingSenderId: "417078899457",
+  appId: "1:417078899457:web:fb2479043950563cb0a100",
+  measurementId: "G-GCZQ560T8E"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+const db = getFirestore();
+
+// TODO : Blog Page
