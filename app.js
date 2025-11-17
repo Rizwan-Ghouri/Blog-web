d83526000e82cbf57709c5bb9328d4d0241c0f8f
Rizwan-Ghouri <RizwanIdrees138@gmail.com>
2025-11-17 08:58:38 +0500

    style path change

--- app.js
+++ app.js
@@ -6,78 +6,46 @@ let npbtn = document.getElementById("np_btn");
 let blogbtn = document.getElementById("blog_btn");
 let blogbtn1 = document.getElementById("blog_btn1");
 
-loginbtn.addEventListener("click" ,()=>{
-    location.href = "/page/login.html";
-})
-loginbtn1.addEventListener("click" ,()=>{
-    location.href = "/page/login.html";
-})
+loginbtn.addEventListener("click", () => {
+  location.href = "page/login.html";
+});
+loginbtn1.addEventListener("click", () => {
+  location.href = "page/login.html";
+});
 
-signupbtn.addEventListener("click" ,()=>{
-    location.href = "page/Signup.html";
-})
-signupbtn1.addEventListener("click" ,()=>{
-    location.href = "page/Signup.html";
-})
+signupbtn.addEventListener("click", () => {
+  location.href = "page/Signup.html";
+});
+signupbtn1.addEventListener("click", () => {
+  location.href = "page/Signup.html";
+});
 
-npbtn.addEventListener("click" ,()=>{
-    location.href = "page/blog.html";
-})
+npbtn.addEventListener("click", () => {
+  location.href = "page/blog.html";
+});
 
-blogbtn.addEventListener("click" ,()=>{
-    location.href = "page/blog.html";
-})
-blogbtn1.addEventListener("click" ,()=>{
-    location.href = "page/blog.html";
-})
+blogbtn.addEventListener("click", () => {
+  location.href = "page/blog.html";
+});
+blogbtn1.addEventListener("click", () => {
+  location.href = "page/blog.html";
+});
 
 let divbtn = document.getElementById("divbtn");
 let menu_btn = document.getElementById("menu_btn");
 let close_btn = document.getElementById("close_btn");
-let divbtn2 = document.getElementById("divbtn2","divbtn");
+let divbtn2 = document.getElementById("divbtn2", "divbtn");
 
+menu_btn.addEventListener("click", () => {
+  divbtn2.style.display = "block";
+  close_btn.style.display = "block";
+  menu_btn.style.display = "none";
+});
 
-
-menu_btn.addEventListener("click",()=>{
-    divbtn2.style.display = "block"
-    close_btn.style.display = "block"
-    menu_btn.style.display = "none"
-})
-
-close_btn.addEventListener("click",()=>{
-    if (menu_btn.style.display = "none") {
-        divbtn2.style.display = "none"
-        close_btn.style.display = "none"
-        menu_btn.style.display = "block"
-    }
-})
-
-
-
-// Todo : connect firebase
-
-// Import the functions you need from the SDKs you need
-// import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
-// import { getFirestore, collection , addDoc  } from "https://www.gstatic.com/firebasejs/11.6.0/firebase/firestore.js";
-// // Todo: Add SDKs for Firebase products that you want to use
-
-// // https://firebase.google.com/docs/web/setup#available-libraries
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDBfllD0_3erohnqzff6scfMENJ_t3tReI",
-//   authDomain: "task-project-c94df.firebaseapp.com",
-//   projectId: "task-project-c94df",
-//   storageBucket: "task-project-c94df.firebasestorage.app",
-//   messagingSenderId: "417078899457",
-//   appId: "1:417078899457:web:fb2479043950563cb0a100",
-//   measurementId: "G-GCZQ560T8E"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// // const analytics = getAnalytics(app);
-// const db = getFirestore();
-
-// TODO : Blog Page
+close_btn.addEventListener("click", () => {
+  if (menu_btn.style.display === "none") {
+    divbtn2.style.display = "none";
+    close_btn.style.display = "none";
+    menu_btn.style.display = "block";
+  }
+});
